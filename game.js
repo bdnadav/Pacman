cb5436f8e293850e2def754942c66665ffe2001e
Harel <harels55@gmail.com>
2019-04-26 22:46:32 +0300

    good ver - register & log in works good
    bibi rules !

--- game.js
+++ game.js
@@ -28,10 +28,22 @@ var last_move;
 
 
 $(".nav-item").on("click", function(){
-    $(".active").removeClass("active");
-    $(this).addClass("active");
+
     let newContent = $(this).text().toLowerCase() + "_content";
     newContent = newContent.replace(/\s/g, '');
+    if ( newContent == "play_content" && "not Logged" == loggedUser){ // move to login
+        alert( "Please log in first :) ")
+        $(".content.show").attr("class", "content hide");
+        let con = document.getElementById("login_content");
+        //$(".active").removeClass("active");
+        $("#play_nav").removeClass("active");
+        $("#login_nav").addClass("active");
+        con.classList.remove("hide");
+        con.classList.add("show");
+        return ;
+    }
+    $(".active").removeClass("active");
+    $(this).addClass("active");
     $(".content.show").attr("class", "content hide");
     let con = document.getElementById(newContent);
     con.classList.remove("hide");
